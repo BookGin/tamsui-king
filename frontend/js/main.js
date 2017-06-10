c5249a24ca37296b4fd4424e264d1fe48d2e7e5e
bookgin <dongsheoil@gmail.com>
2017-06-10 12:41:03 +0800

    Make variable names camelCase

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,7 +1,7 @@
 var map;
 var player;
 var directionsService;
-var starting_position = {"lat":25.019422934847636, "lng":121.5412656654205};
+var startingPosition = {"lat":25.019422934847636, "lng":121.5412656654205};
 
 // Reference: https://stackoverflow.com/a/32784450
 function point2LatLng(point) {
@@ -14,14 +14,14 @@ function point2LatLng(point) {
 
 // Reference: https://stackoverflow.com/questions/16180104/get-a-polyline-from-google-maps-directions-v3
 document.documentElement.addEventListener('click', function(mouse) {
-  var client_point = {x: mouse.clientX, y:mouse.clientY};
-  //debug("Click " + JSON.stringify(client_point));
-	var new_position = point2LatLng(client_point);
-  debug("Click on" + JSON.stringify(new_position));
+  var clientPoint = {x: mouse.clientX, y:mouse.clientY};
+  //debug("Click " + JSON.stringify(clientPoint));
+	var newPosition = point2LatLng(clientPoint);
+  debug("Click on" + JSON.stringify(newPosition));
 
   directionsService.route({
     origin: player.getPosition(),
-    destination: new_position,
+    destination: newPosition,
     travelMode: google.maps.TravelMode.WALKING
   }, function(response, status) {
     if (status == google.maps.DirectionsStatus.OK) {
@@ -34,19 +34,19 @@ document.documentElement.addEventListener('click', function(mouse) {
       var legs = response.routes[0].legs;
       for (let leg of legs) {
         for (let step of leg.steps) {
-          for (let next_pos of step.path) {
-            polyline.getPath().push(next_pos);
+          for (let nextPos of step.path) {
+            polyline.getPath().push(nextPos);
             new google.maps.Marker({
               map: map,
-              position: next_pos,
+              position: nextPos,
             });
           }
         }
       }
       polyline.setMap(map);
 
-      player.setPosition(new_position);
-      map.panTo(new_position);
+      player.setPosition(newPosition);
+      map.panTo(newPosition);
     } else {
       window.alert('Directions request failed due to ' + status);
     }
@@ -60,7 +60,7 @@ function debug(msg){
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: starting_position,
+    center: startingPosition,
     zoom: 18,
   });
 
@@ -68,7 +68,7 @@ function initMap() {
 
 	player = new google.maps.Marker({
 		map: map,
-		position: starting_position,
+		position: startingPosition,
 		title: 'Hello World!',
 		icon: "http://maps.google.com/mapfiles/ms/micons/woman.png",
 	});
