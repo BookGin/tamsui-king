004c9796b1da92f7a75fe864614f9592918f035d
Chia-sheng Chen <qitar888@gmail.com>
2017-06-11 15:08:31 +0800

    Add root

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   resource :map, only: [:show]
 
   get 'dumb_room/show'
-
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount ActionCable.server => "/cable"
+  root to: "sessions#new"
 end
