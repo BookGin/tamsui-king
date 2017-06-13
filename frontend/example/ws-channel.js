a2e07b94733e9d141429314a1d0154186e26f6a8
bookgin <dongsheoil@gmail.com>
2017-06-13 11:10:38 +0800

    Add server to client API

--- frontend/example/ws-channel.js
+++ frontend/example/ws-channel.js
@@ -1,7 +1,7 @@
 ActionCable.startDebugging()
 window.App = {}
 window.App.cable = ActionCable.createConsumer("ws://localhost:12345/cable")
-window.App.clockChannel = window.App.cable.subscriptions.create({channel: "WsChannel", uuid: randomInt()}, {
+window.App.wsChannel = window.App.cable.subscriptions.create({channel: "WsChannel", uuid: randomInt()}, {
   // ActionCable callbacks
   connected: function() {
     debug("websocket start " + String(this.identifier));
@@ -13,9 +13,17 @@ window.App.clockChannel = window.App.cable.subscriptions.create({channel: "WsCha
     debug("websocket is rejected");
   },
   received: function(data) {
-    debug("Receive data: " + String(data));
-    this.tick(data)
+    debug("Receive data: " + JSON.stringify(data));
+    if (data.action === "bomb_positions")
+      this.updateBombPositions(data);
+    else if (data.action === "bomb_explosion")
+      this.bombExplode(data);
+    else if (data.action === "die")
+      this.die(data);
+    else if (data.action === "player_positions")
+      this.updatePlayerPositions(data);
   },
+
   // Custom methods
 
   // client -> server
@@ -26,7 +34,30 @@ window.App.clockChannel = window.App.cable.subscriptions.create({channel: "WsCha
   plantBomb: function(playerName, latLngPosition) {
     this.perform("plantBomb", { player: playerName, position: latLngPosition, nonce: randomInt()});
   }
-  // TODO: server -> client
+
+  // server -> client
+  updateBombPositions: function(data) {
+    for (let bombPosition of data.positions) {
+      // TODO
+    }
+  }
+
+  bombExplode: function(data) {
+    let bombPosition = data.position;
+    let radius = data.radius;
+    // TODO
+  }
+
+  die: function(data) {
+    let playerName = data.player;
+    // TODO
+  }
+
+  updatePlayerPositions: function(data) {
+    for (let playerPosition of data.positions) {
+      // TODO
+    }
+  }
 });
 
 function randomInt() {
